46bed55d9d9d007e24cc40fb255c1e63182388b5
Sudan Shakya <sudanshakya08@gmail.com>
2022-12-15 21:10:20 +0545

    added form validation

--- src/components/login/SignupUI.jsx
+++ src/components/login/SignupUI.jsx
@@ -1,7 +1,14 @@
-import { Box, Button, FormControl, FormLabel, HStack, Image, Input, Text, VStack } from '@chakra-ui/react';
+import { Box, Button, FormControl, FormErrorMessage, FormLabel, HStack, Image, Input, Text, VStack } from '@chakra-ui/react';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const SignupUI = () => {
+
+    const {handleSubmit,register,formState:{errors}}= useForm();
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+
+    // }
     return (
         <Box h={"100vh"} w={"100vw"} display={"flex"} justifyContent={"center"} alignItems={"center"}>
             <Box boxShadow={"rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;"} h={"70vh"} w={"50vw"}>
@@ -9,11 +16,36 @@ const SignupUI = () => {
                     <Box>
                         <VStack w={"25vw"} h={"70vh"} justifyContent={"center"} alignItems={"center"}>
                             <Text>Login</Text>
-                            <FormControl is isRequired>
-                                <FormLabel as="email"></FormLabel>
-                                <Input placeholder='Enter your email' />
-                            </FormControl>
-                            <Button >Login</Button>
+                            <form>
+                                <FormControl isInvalid={errors.email}>
+                                    <FormLabel as="email" />
+                                    <Input id ="email" type="email" placeholder='Enter your email'
+                                        {...register("email",{
+                                            required:'Email is required',
+                                            pattern:/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                                            message:"Invalid email address"
+                                        })}/>
+                                    <FormErrorMessage>
+                                        {errors.email && errors.email.message}
+                                    </FormErrorMessage>
+                                    
+                                </FormControl>
+
+                                <FormControl isInvalid={errors.password}>
+                                    <FormLabel as="password" />
+                                    <Input id ="password" type="password" placeholder='Enter your password'
+                                        {...register("password",{
+                                            required:'Password is required',
+                                        })}/>
+                                    <FormErrorMessage>
+                                        {errors.password && errors.password.message}
+                                    </FormErrorMessage>
+                                    
+                                </FormControl>
+
+                                
+                            </form>
+                            <Button onClick={handleSubmit("#")}>Login</Button>
 
                         </VStack>
                     </Box>
