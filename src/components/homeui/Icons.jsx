f7785c0977091311d9699ad8cbea97c319215250
Sudan Shakya <sudanshakya08@gmail.com>
2023-01-20 20:42:38 +0545

    added search bar

--- src/components/homeui/Icons.jsx
+++ src/components/homeui/Icons.jsx
@@ -1,15 +1,26 @@
-import { Box, HStack, Icon, useColorMode } from "@chakra-ui/react";
+import { Box, HStack, Icon, Input, useColorMode } from "@chakra-ui/react";
+import { useState } from "react";
 import { AiOutlineHeart, AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
 import { BsMoon, BsSun } from "react-icons/bs";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Icons = () =>{
+
+
+    const [search,setSearch] = useState({
+        clicked : false,
+        searchTerm : '',
+    });
+    console.log(search);
     const { colorMode, toggleColorMode } = useColorMode();
     const totalItems = useSelector((state)=>state.cart.totalItems);
+
+
     return <>
         <HStack  gap={'10px'}>
-            <Icon as={AiOutlineSearch} h={'25px'} w={'25px'} />
+            <Icon as={AiOutlineSearch} h={'25px'} w={'25px'} onClick={()=>setSearch(...search.clicked === true )}/>
+            {search.clicked?<Input placeholder="Search..." onChange={(e)=>setSearch(...search.searchTerm === e.target.value)}/>:null}
             <Icon as={AiOutlineHeart} h={'25px'} w={'25px'} />
             <Link to={'/Cart'} >
                 <Icon as={AiOutlineShoppingCart} h={'25px'} w={'25px'} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import NavBar from '../components/NavBar';
 import Grids from '../components/homeui/Grids';
 import Products from '../components/homeui/Products';
 import Slider from '../components/homeui/Slider';
+import { Table, Td, Tr } from '@chakra-ui/react';
 
 
 
 const Home = () => {
     return (
         <>
+            
             <NavBar />
             <Grids />
             <Products />
