4866e7d23a315137341d09f6910e6635c3856b19
Sudan Shakya <sudanshakya08@gmail.com>
2023-01-23 21:14:28 +0545

    fixed search bar problem

--- src/components/homeui/Icons.jsx
+++ src/components/homeui/Icons.jsx
@@ -6,21 +6,26 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Icons = () =>{
-
-
     const [search,setSearch] = useState({
         clicked : false,
         searchTerm : '',
     });
-    console.log(search);
     const { colorMode, toggleColorMode } = useColorMode();
     const totalItems = useSelector((state)=>state.cart.totalItems);
+
+    const handleClick = () =>{
+        setSearch({...search,clicked : !search.clicked});
+    };
+    const handleChange = (e) =>{
+        setSearch({...search,searchTerm : e.target.value});
+    };
 
 
     return <>
         <HStack  gap={'10px'}>
-            <Icon as={AiOutlineSearch} h={'25px'} w={'25px'} onClick={()=>setSearch(...search.clicked === true )}/>
-            {search.clicked?<Input placeholder="Search..." onChange={(e)=>setSearch(...search.searchTerm === e.target.value)}/>:null}
+            <Icon as={AiOutlineSearch} h={'25px'} w={'25px'} onClick={handleClick}/>
+            {console.log(search.clicked)}
+            {search.clicked?<Input placeholder="Search..." onChange={(e)=>{handleChange(e);}}/>:null}
             <Icon as={AiOutlineHeart} h={'25px'} w={'25px'} />
             <Link to={'/Cart'} >
                 <Icon as={AiOutlineShoppingCart} h={'25px'} w={'25px'} />
