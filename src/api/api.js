65e05e79da6fb639cb771baeccd33bbc838d0a16
SUDAN_LAPTOP\sudan <sudanshakya08@gmail.com>
2023-02-24 11:09:54 +0545

    changed api endpoint

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,4 @@
-export const BASEURL = 'https://strapi-production-0f2b.up.railway.app/';
+// export const BASEURL = 'https://strapi-production-0f2b.up.railway.app/';
+export const BASEURL = 'http://localhost:1337';
+
+
